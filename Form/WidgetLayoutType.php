afa591b8fa188ad73e1d32e0be793353e89f57f6
Leny BERNARD <leny@appventus.com>
2016-02-22 23:55:01 +0100

    :arrow_up: ~2.8|~3.0 symfony

--- Form/WidgetLayoutType.php
+++ Form/WidgetLayoutType.php
@@ -2,10 +2,10 @@
 
 namespace Victoire\Widget\LayoutBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Victoire\Bundle\CoreBundle\Form\WidgetType;
-use Victoire\Widget\LayoutBundle\Entity\WidgetLayout;
 
 /**
  * WidgetLayout form type.
@@ -22,41 +22,45 @@ class WidgetLayoutType extends WidgetType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $choices = [
-            'once'                 => 'widget_layout.form.layout.choice.once.label',
-            'third'                => 'widget_layout.form.layout.choice.third.label',
-            'third12'              => 'widget_layout.form.layout.choice.third12.label',
-            'third21'              => 'widget_layout.form.layout.choice.third21.label',
-            'half'                 => 'widget_layout.form.layout.choice.half.label',
-            'halfquarterquarter'   => 'widget_layout.form.layout.choice.halfquarterquarter.label',
-            'quarterhalfquarter'   => 'widget_layout.form.layout.choice.quarterhalfquarter.label',
-            'quarterquarterhalf'   => 'widget_layout.form.layout.choice.quarterquarterhalf.label',
-            'quarters'             => 'widget_layout.form.layout.choice.quarters.label',
-            'fifth'                => 'widget_layout.form.layout.choice.fifth.label',
-            '2575'                 => 'widget_layout.form.layout.choice.2575.label',
-            '7525'                 => 'widget_layout.form.layout.choice.7525.label',
-            'sixth'                => 'widget_layout.form.layout.choice.sixth.label',
+            'widget_layout.form.layout.choice.once.label'               => 'once',
+            'widget_layout.form.layout.choice.third.label'              => 'third',
+            'widget_layout.form.layout.choice.third12.label'            => 'third12',
+            'widget_layout.form.layout.choice.third21.label'            => 'third21',
+            'widget_layout.form.layout.choice.half.label'               => 'half',
+            'widget_layout.form.layout.choice.halfquarterquarter.label' => 'halfquarterquarter',
+            'widget_layout.form.layout.choice.quarterhalfquarter.label' => 'quarterhalfquarter',
+            'widget_layout.form.layout.choice.quarterquarterhalf.label' => 'quarterquarterhalf',
+            'widget_layout.form.layout.choice.quarters.label'           => 'quarters',
+            'widget_layout.form.layout.choice.fifth.label'              => 'fifth',
+            'widget_layout.form.layout.choice.2575.label'               => '2575',
+            'widget_layout.form.layout.choice.7525.label'               => '7525',
+            'widget_layout.form.layout.choice.sixth.label'              => 'sixth',
         ];
-        $builder->add('layoutXs', 'choice', [
+        $builder->add('layoutXs', ChoiceType::class, [
             'label'        => 'widget_layout.form.layoutXs.label',
             'choices'      => $choices,
+            'choices_as_values' => true,
             'required'     => false,
             'placeholder'  => 'widget_layout.form.layout.placeholder',
         ])
-        ->add('layoutSm', 'choice', [
+        ->add('layoutSm', ChoiceType::class, [
             'label'        => 'widget_layout.form.layoutSm.label',
             'choices'      => $choices,
+            'choices_as_values' => true,
             'required'     => false,
             'placeholder'  => 'widget_layout.form.layout.placeholder',
         ])
-        ->add('layoutMd', 'choice', [
+        ->add('layoutMd', ChoiceType::class, [
             'label'        => 'widget_layout.form.layoutMd.label',
             'choices'      => $choices,
+            'choices_as_values' => true,
             'required'     => true,
             'placeholder'  => 'widget_layout.form.layout.placeholder',
         ])
-        ->add('layoutLg', 'choice', [
+        ->add('layoutLg', ChoiceType::class, [
             'label'        => 'widget_layout.form.layoutLg.label',
             'choices'      => $choices,
+            'choices_as_values' => true,
             'required'     => false,
             'placeholder'  => 'widget_layout.form.layout.placeholder',
         ])
@@ -69,28 +73,16 @@ class WidgetLayoutType extends WidgetType
     }
 
     /**
-     * bind form to WidgetLayout entity.
-     *
-     * @param OptionsResolverInterface $resolver
+     * {@inheritdoc}
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
-        parent::setDefaultOptions($resolver);
+        parent::configureOptions($resolver);
 
         $resolver->setDefaults([
             'data_class'         => 'Victoire\Widget\LayoutBundle\Entity\WidgetLayout',
             'widget'             => 'Layout',
             'translation_domain' => 'victoire',
         ]);
-    }
-
-    /**
-     * get form name.
-     *
-     * @return string The form name
-     */
-    public function getName()
-    {
-        return 'victoire_widget_form_layout';
     }
 }
